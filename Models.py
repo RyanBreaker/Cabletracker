4e1d1186665f4285b15eb089d38ac2afbe373d02
Ryan Breaker <ryan@breaker.rocks>
2018-03-18 20:57:58 -0500

    Model functionality

--- Models.py
+++ Models.py
@@ -1,10 +1,37 @@
-from sqlalchemy import Column, Integer, String, ForeignKey
-from sqlalchemy.orm import validates, relationship
+from sqlalchemy import Column, Integer, String, ForeignKey, or_
+from sqlalchemy.orm import validates, relationship, Query, Session
 from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
 
+### Model Exceptions
+class ModelException(Exception):
+    message = None
 
+    def __str__(self):
+        return self.message
+
+
+class SamePortException(ModelException):
+    message = "The given Ports are the same."
+
+
+class PortFullException(ModelException):
+    def __init__(self, port: "Port"):
+        self.message = "'{}' is already full.".format(port.name)
+
+
+class LinkExistsException(ModelException):
+    def __init__(self, ports: ["Port"]):
+        self.message = "A link already exists between ports '{}' and '{}'.".format(ports[0].name, ports[1].name)
+
+
+class LinkDoesntExistException(ModelException):
+    def __init__(self, ports: ["Port"]):
+        self.message = "No link between '{}' and '{}' exists.".format(ports[0].name, ports[1].name)
+
+
+### Models
 class Port(Base):
     __tablename__ = 'ports'
 
@@ -13,7 +40,7 @@ class Port(Base):
     description = Column(String, default='', nullable=False)
 
     @validates('name')
-    def validate_name(self, _, name):
+    def validate_name(self, _, name: str):
         name = name.upper()
         assert len(name) > 0
         assert name.startswith(('PP', 'FP', 'RR'))
@@ -22,6 +49,70 @@ class Port(Base):
     def __repr__(self):
         return "<Port(name='{}', description='{}')>".format(self.name, self.description)
 
+    def links(self, session: Session) -> ["Link"]:
+        return session.query(Link).filter(
+            or_(
+                Link.end_a_id == self.id,
+                Link.end_b_id == self.id
+            )
+        ).order_by(Link.id).all()
+
+    def full(self, session: Session) -> bool:
+        return len(self.links(session)) is 2
+
+    def linked_to(self, session: Session, port: "Port"):
+        links = self.links(session)
+        while links:
+            link = links.pop()
+            if link.end_a is port or link.end_b is port:
+                return True
+        return False
+
+    @staticmethod
+    def create_link(session: Session, port_a: "Port", port_b: "Port") -> "Link":
+        if port_a is port_b:
+            raise SamePortException()
+
+        existing_a = port_a.links(session)
+        existing_b = port_b.links(session)
+
+        # Make sure ports aren't full
+        if port_a.full(session):
+            raise PortFullException(port_a)
+        if port_b.full(session):
+            raise PortFullException(port_b)
+
+        # Check for preexisting connections
+        if len(existing_a) is 1:
+            link = existing_a[0]
+            if link.end_a is port_b or link.end_b is port_b:
+                raise LinkExistsException([port_a, port_b])
+
+        if len(existing_b) is 1:
+            link = existing_b[0]
+            if link.end_a is port_a or link.end_b is port_a:
+                raise LinkExistsException([port_a, port_b])
+
+        link = Link(end_a=port_a, end_b=port_b)
+        session.add(link)
+
+        return link
+
+    @staticmethod
+    def remove_link(session: Session, port_a: "Port", port_b: "Port") -> "Link":
+        if port_a is port_b:
+            raise SamePortException()
+
+        links = port_a.links(session)
+        while links:
+            link = links.pop()
+            if link.end_a is port_b or link.end_b is port_b:
+                session.delete(link)
+                return link
+
+        # If nothing was returned, then no link existed nor could be deleted
+        raise LinkDoesntExistException([port_a, port_b])
+
 
 class Link(Base):
     __tablename__ = 'links'
@@ -29,14 +120,18 @@ class Link(Base):
     id = Column(Integer, primary_key=True)
     description = Column(String, default='', nullable=False)
 
-    end_a_id = Column(Integer, ForeignKey('ports.id'), unique=True)
-    end_b_id = Column(Integer, ForeignKey('ports.id'), unique=True)
-    end_a = relationship('Port', foreign_keys=end_a_id, backref='side_a')
-    end_b = relationship('Port', foreign_keys=end_b_id, backref='side_b')
+    end_a_id = Column(Integer, ForeignKey('ports.id'), index=True)
+    end_b_id = Column(Integer, ForeignKey('ports.id'), index=True)
+    end_a = relationship('Port', foreign_keys=end_a_id)
+    end_b = relationship('Port', foreign_keys=end_b_id)
 
     @property
-    def serial(self):
+    def serial(self) -> str:
         return '{:06x}'.format(self.id)
+
+    @property
+    def full(self) -> bool:
+        return self.end_a and self.end_b
 
     def __repr__(self):
         end_a = 'None' if self.end_a is None else self.end_a.name
--- main.py
+++ main.py
@@ -4,26 +4,29 @@ from sqlalchemy.orm import sessionmaker
 from Models import *
 from Commands import *
 
-engine = create_engine('sqlite:///:memory:', echo=True)
+engine = create_engine('sqlite:///:memory:')
 session = sessionmaker(bind=engine)()
 
 Base.metadata.create_all(engine)
 
-# p1 = Port(name='PP.1:01')
-# p2 = Port(name='PP.2:01')
+p1 = Port(name='PP.1:01')
+p2 = Port(name='PP.2:01')
+p3 = Port(name='PP.3:01')
+
+session.add_all([p1, p2, p3])
+Port.create_link(session, p1, p2)
+Port.create_link(session, p1, p2)
+
+print(session.query(Port).all())
+print(session.query(Link).all())
+print(p2.links(session))
+
+# PROMPT = '> '
 #
-# session.add_all([p1, p2])
-# session.add(Link(end_a=p1, end_b=p2))
 #
-# print(session.query(Port).all())
-# print(session.query(Link).all())
-
-PROMPT = '> '
-
-
-while True:
-    response = input(PROMPT).split()
-    if len(response) is 0:
-        continue
-    if response[0] == 'help':
-        print(cmd_help())
+# while True:
+#     response = input(PROMPT).split()
+#     if len(response) is 0:
+#         continue
+#     if response[0] == 'help':
+#         print(cmd_help())
