0ba33f4dabd03da46aed0868a3efd54bfb3b3ccf
Ryan Breaker <ryan@breaker.rocks>
2018-03-19 17:08:05 -0500

    Move session-based processes out of Models

--- source/Models.py
+++ source/Models.py
@@ -1,13 +1,16 @@
-from sqlalchemy import Column, Integer, String, ForeignKey, or_
-from sqlalchemy.orm import validates, relationship, Query, Session
+import contextlib
+
+from sqlalchemy import Column, Integer, String, ForeignKey, or_, and_
+from sqlalchemy.orm import validates, relationship, Session
 from sqlalchemy.ext.declarative import declarative_base
 
 __all__ = ["ModelException", "PortBadNameException", "SamePortException", "PortFullException", "LinkExistsException",
-           "LinkDoesntExistException", "Link", "Port", "Base"]
+           "LinkDoesntExistException", "Link", "Port", "ModelUtil", "Base"]
 
 Base = declarative_base()
 
-### Model Exceptions
+
+# Model Exceptions
 class ModelException(Exception):
     message = None
 
@@ -29,20 +32,20 @@ class PortFullException(ModelException):
 
 
 class LinkExistsException(ModelException):
-    def __init__(self, ports: ["Port"]):
-        self.message = "A link already exists between ports '{}' and '{}'.".format(ports[0].name, ports[1].name)
+    def __init__(self, port_a: "Port", port_b: "Port"):
+        self.message = "A link already exists between ports '{}' and '{}'.".format(port_a.name, port_b.name)
 
 
 class LinkDoesntExistException(ModelException):
-    def __init__(self, ports: ["Port"]):
-        self.message = "No link between '{}' and '{}' exists.".format(ports[0].name, ports[1].name)
+    def __init__(self, port_a: "Port", port_b: "Port"):
+        self.message = "No link between '{}' and '{}' exists.".format(port_a.name, port_b.name)
 
 
-### Models
+# Models
 class Port(Base):
     __tablename__ = 'ports'
 
-    id = Column(Integer, primary_key=True)
+    id: Column = Column(Integer, primary_key=True)
     name = Column(String, unique=True, nullable=False)
     description = Column(String, default='', nullable=False)
 
@@ -56,69 +59,8 @@ class Port(Base):
     def __repr__(self):
         return "<Port(name='{}', description='{}')>".format(self.name, self.description)
 
-    def links(self, session: Session) -> ["Link"]:
-        return session.query(Link).filter(
-            or_(
-                Link.end_a_id == self.id,
-                Link.end_b_id == self.id
-            )
-        ).order_by(Link.id).all()
-
-    def full(self, session: Session) -> bool:
-        return len(self.links(session)) is 2
-
-    def linked_to(self, session: Session, port: "Port"):
-        links = self.links(session)
-        while links:
-            link = links.pop()
-            if link.end_a is port or link.end_b is port:
-                return True
-        return False
-
-    @staticmethod
-    def create_link(session: Session, port_a: "Port", port_b: "Port") -> "Link":
-        if port_a is port_b:
-            raise SamePortException()
-
-        existing_a = port_a.links(session)
-        existing_b = port_b.links(session)
-
-        # Make sure ports aren't full
-        if port_a.full(session):
-            raise PortFullException(port_a)
-        if port_b.full(session):
-            raise PortFullException(port_b)
-
-        # Check for preexisting connections
-        if len(existing_a) is 1:
-            link = existing_a[0]
-            if link.end_a is port_b or link.end_b is port_b:
-                raise LinkExistsException([port_a, port_b])
-
-        if len(existing_b) is 1:
-            link = existing_b[0]
-            if link.end_a is port_a or link.end_b is port_a:
-                raise LinkExistsException([port_a, port_b])
-
-        link = Link(end_a=port_a, end_b=port_b)
-        session.add(link)
-
-        return link
-
-    @staticmethod
-    def remove_link(session: Session, port_a: "Port", port_b: "Port") -> "Link":
-        if port_a is port_b:
-            raise SamePortException()
-
-        links = port_a.links(session)
-        while links:
-            link = links.pop()
-            if link.end_a is port_b or link.end_b is port_b:
-                session.delete(link)
-                return link
-
-        # If nothing was returned, then no link existed nor could be deleted
-        raise LinkDoesntExistException([port_a, port_b])
+    def __hash__(self):
+        return hash(self.name)
 
 
 class Link(Base):
@@ -127,20 +69,98 @@ class Link(Base):
     id = Column(Integer, primary_key=True)
     description = Column(String, default='', nullable=False)
 
-    end_a_id = Column(Integer, ForeignKey('ports.id'), index=True)
-    end_b_id = Column(Integer, ForeignKey('ports.id'), index=True)
+    end_a_id: Column = Column(Integer, ForeignKey('ports.id'), index=True, nullable=False)
+    end_b_id: Column = Column(Integer, ForeignKey('ports.id'), index=True, nullable=False)
     end_a = relationship('Port', foreign_keys=end_a_id)
     end_b = relationship('Port', foreign_keys=end_b_id)
 
     @property
     def serial(self) -> str:
+        """
+        :return: Serial of the Link, in Hex form
+        """
         return '{:06x}'.format(self.id)
-
-    @property
-    def full(self) -> bool:
-        return self.end_a and self.end_b
 
     def __repr__(self):
         end_a = 'None' if self.end_a is None else self.end_a.name
         end_b = 'None' if self.end_b is None else self.end_b.name
         return "<Link(serial='{}', port_a='{}', port_b='{}')>".format(self.serial, end_a, end_b)
+
+
+class ModelUtil:
+    """
+    Utility class for running operations on the Port and Link models with the given Session.
+    """
+    session = None
+
+    def __init__(self, session: Session):
+        self.session = session
+
+    def all_ports(self) -> [Port]:
+        """
+        :return: A list of all Ports in the database
+        """
+        return self.session.query(Port).order_by(Port.name).all()
+
+    def empty_ports(self) -> [Port]:
+        """
+        :return: All empty Ports (no Links connected)
+        """
+        q = self.session.query(Port)
+
+
+    def all_links(self) -> [Link]:
+        """
+        :return: A list of all Links in the database
+        """
+        return self.session.query(Link).order_by(Link.id).all()
+
+    def port_links(self, port: Port):
+        """
+        Returns all links relevant to the given Port
+        :param port: Port to search for connected links
+        :return: A list of Links connected to the given Port.
+        """
+        return self.session.query(Link).filter(
+            or_(
+                Link.end_a_id == port.id,
+                Link.end_b_id == port.id
+            )).order_by(Link.id).all()
+
+    def port_connection(self, port_a: Port, port_b: Port) -> Link:
+        """
+        Returns the Link that connects the given Ports, raises LinkDoesntExistException if none exist.
+        :return: The Link that connects the given Ports
+        """
+        if port_a is port_b:
+            raise SamePortException()
+
+        port_a_links = self.port_links(port_a)
+        while port_a_links:
+            link = port_a_links.pop()
+            if link.end_a is port_b or link.end_b is port_b:
+                return link
+        raise LinkDoesntExistException(port_a, port_b)
+
+    def link_exists(self, port_a: Port, port_b: Port) -> bool:
+        """
+        Convenience function for checking whether a link exists between two Ports
+        :return: True if a link exists, False otherwise
+        """
+        try:
+            self.port_connection(port_a, port_b)
+            return True
+        except LinkDoesntExistException:
+            return False
+
+    def create_link(self, port_a: Port, port_b: Port) -> Link:
+        """
+        Creates a Link connecting the given port_a and port_b, raises LinkExistsException if a Link already exists
+        :return: The Link that was created
+        """
+        if self.link_exists(port_a, port_b):
+            raise LinkExistsException(port_a, port_b)
+
+        link = Link(end_a=port_a, end_b=port_b)
+        self.session.add(link)
+        return link
--- source/tests/ModelTests.py
+++ source/tests/ModelTests.py
@@ -1,4 +1,5 @@
 import unittest
+from collections import Counter
 
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, Session
@@ -12,13 +13,16 @@ def gen_ports() -> {str: Port}:
 
 
 class ModelTest(unittest.TestCase):
+
     def setUp(self):
         self.engine = create_engine('sqlite:///:memory:')
         self.session: Session = sessionmaker(bind=self.engine)()
         Base.metadata.create_all(self.engine)
+        self.util = ModelUtil(self.session)
 
 
 class PortTest(ModelTest):
+
     def test_validate_name(self):
         port = Port(name=' pp.1:01  ')
         # Verify upper() and strip() worked
@@ -32,48 +36,26 @@ class PortTest(ModelTest):
         with self.assertRaises(PortBadNameException):
             Port(name='foo')
 
-    def test_links(self):
-        pass
 
-    def test_full(self):
+class ModelUtilTest(ModelTest):
+
+    def test_all_ports(self):
         ports = gen_ports()
         self.session.add_all(ports.values())
+        ports_query = self.util.all_ports()
+
+        self.assertTrue(Counter(ports.values()) == Counter(ports_query))
+
+    def test_empty_ports(self):
+        ports = gen_ports()
+        self.session.add_all(ports.values())
+        ports_query = self.util.empty_ports()
 
         p1, p2, p3, p4 = ports.values()
-        Port.create_link(self.session, p1, p2)
-        Port.create_link(self.session, p2, p3)
 
-        self.assertFalse(p1.full(self.session))
-        self.assertTrue(p2.full(self.session))
-        self.assertFalse(p3.full(self.session))
+        self.assertTrue(Counter([p1, p2, p3, p4]) == Counter(ports_query))
 
+        self.util.create_link(p1, p2)
+        ports_query = self.util.empty_ports()
 
-    def test_port_create_link(self):
-        p1 = Port(name='PP.1:01')
-        p2 = Port(name='PP.2:01')
-        p3 = Port(name='PP.3:01')
-        p4 = Port(name='PP.4:01')
-
-        self.session.add_all([p1, p2, p3, p4])
-        link_1 = Port.create_link(self.session, p1, p2)
-
-        self.assertEqual(link_1.end_a, p1)
-        self.assertEqual(link_1.end_b, p2)
-
-        with self.assertRaises(SamePortException):
-            Port.create_link(self.session, p1, p1)
-
-        with self.assertRaises(LinkExistsException):
-            Port.create_link(self.session, p1, p2)
-
-        link_2 = Port.create_link(self.session, p2, p3)
-
-        self.assertEqual(link_2.end_a, p2)
-        self.assertEqual(link_2.end_b, p3)
-
-        with self.assertRaises(PortFullException):
-            Port.create_link(self.session, p2, p4)
-
-
-if __name__ == '__main__':
-    unittest.main()
+        self.assertTrue(Counter([p3, p4]) == Counter(ports_query))
