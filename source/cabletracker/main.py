e6c4314a7649087a872e4491a5940625c30d176a
Ryan Breaker <ryan@breaker.rocks>
2018-03-22 13:24:30 -0500

    Cleanup

--- source/cabletracker/main.py
+++ source/cabletracker/main.py
@@ -1,31 +1,18 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-from Models import *
+from .Models import *
 
 engine = create_engine('sqlite:///:memory:')
 session = sessionmaker(bind=engine)()
-
 Base.metadata.create_all(engine)
+
+util = ModelUtil(session)
 
 p1 = Port(name='PP.1:01')
 p2 = Port(name='PP.2:01')
 p3 = Port(name='PP.3:01')
 
 session.add_all([p1, p2, p3])
-Port.create_link(session, p1, p2)
-Port.create_link(session, p1, p2)
-
-print(session.query(Port).all())
-print(session.query(Link).all())
-print(p2.links(session))
-
-# PROMPT = '> '
-#
-#
-# while True:
-#     response = input(PROMPT).split()
-#     if len(response) is 0:
-#         continue
-#     if response[0] == 'help':
-#         print(cmd_help())
+util.create_connection(p1, p2)
+util.create_connection(p2, p3)
